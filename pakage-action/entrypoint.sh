7a73c11233b6e33ccf23791d40faa61eb584f8c5
For Register <forregister@outlook.es>
2021-04-09 01:19:09 -0500

    Fix messages on entrypoint.sh

--- pakage-action/entrypoint.sh
+++ pakage-action/entrypoint.sh
@@ -9,7 +9,7 @@ chown -R nobody:nobody shim-signed && cd shim-signed
 # Making package
 sudo -u nobody makepkg -C --noconfirm
 
-#
+echo "Copying shim-signed package"
 cp shim-signed-15.f8-2-any.pkg.tar.zst /github/workspace/
 
 
