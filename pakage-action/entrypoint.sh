397794bacae8468434b882b7eff821a8deecdc0b
For Register <forregister@outlook.es>
2021-04-09 01:00:36 -0500

    Github actions test

---
+++ pakage-action/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Package clone
+cd /tmp && git clone https://aur.archlinux.org/shim-signed.git
+
+# 
+chown -R nobody:nobody shim-signed && cd shim-signed
+
+# Making package
+sudo -u nobody makepkg -C --noconfirm
+
+#
+cp shim-signed-15.f8-2-any.pkg.tar.zst /github/workspace/
+
+
+
+
+
