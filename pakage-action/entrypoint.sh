53ef2aef1ea3b649d3d877d77fe2cdfc99d9469f
For Register <forregister@outlook.es>
2021-04-29 21:40:50 -0500

    Added submodule 'shim-signed' repo

--- pakage-action/entrypoint.sh
+++ pakage-action/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Making shim-signed package"
-cd /tmp && git clone https://aur.archlinux.org/shim-signed.git
 chown -R nobody:nobody shim-signed && cd shim-signed
 sudo -u nobody makepkg -C --noconfirm
 
--- test-action/entrypoint.sh
+++ test-action/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Listing packages"
-ls -la /github/workspace/
+echo "'shim-signed' package exists?"
+count=$(ls | grep -ci shim-signed-*.zst)
+[[ ${count} -eq 0 ]] && exit 1
 
 exit 0
